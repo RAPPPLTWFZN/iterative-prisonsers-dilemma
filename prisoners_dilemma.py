3e91f8f94f1339625c7779bf54e715e5d0f7eb1b
Tom-Boy <isaachodg@gmail.com>
2014-11-18 11:28:50 -0600

    Isaac_Hodge_Player4_Update

--- prisoners_dilemma.py
+++ prisoners_dilemma.py
@@ -191,15 +191,14 @@ def get_action(player, history, opponent_history, score, opponent_score, getting
     #
     elif player == 4:
         if getting_team_name:
-            return 'betray every 3rd round'
+            return 'Hello'
         else:
             # use history, opponent_history, score, opponent_score
             # to compute your strategy
             size = len(history)
-            if(size%3==0): #the number of rounds played is a multiple of 3
-                return 'c'
-            else:
-                return 'b'
+        if(size%10000==0): #the number of rounds played is a multiple of 3
+            return 'c'
+
     
     
     
