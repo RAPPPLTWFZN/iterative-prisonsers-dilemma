cc4c64ad1f07eee2212e93fd43d232ba6e39c484
AquaticDark <jrhod99@gmail.com>
2014-11-18 11:13:57 -0600

    Jared Rhodes' Player 8 update v.1

--- prisoners_dilemma.py
+++ prisoners_dilemma.py
@@ -303,31 +303,21 @@ def get_action(player, history, opponent_history, score, opponent_score, getting
     elif player == 8:
         if getting_team_name:
             #if there was a previous round just like 
-            return 'loyal vengeful with permanent second impression'
+            return 'I WILL WIN EVERYTHING'
         else:
             # use history, opponent_history, score, opponent_score
             # to compute your strategy      
-            if len(opponent_history)==0: #It's the first round: collude
-                return 'c'
-            else:
-                # if there was a previous round just like the last one,
-                # do whatever they did in the round that followed it
-                recent_round_opponent = opponent_history[-1]
-                recent_round_me = history[-1]
-                            
-                #go through rounds before that one
-                for round in range(len(history)-1):
-                    prior_round_opponent = opponent_history[round]
-                    prior_round_me = history[round]
-                    #if one matches
-                    if (prior_round_me == recent_round_me) and \
-                            (prior_round_opponent == recent_round_opponent):
-                        return opponent_history[round]
+            if len(opponent_history)==0: #It's the first round: betray
+                return 'b'
                 # no match found
                 if history[-1]=='c' and opponent_history[-1]=='b':
                     return 'b' # betray is they were severely punished last time
+                elif opponent_history[-1]=='c':
+                    return 'b'
+                elif history[-1]=='b' and opponent_history[-1]=='c':
+                    return 'b' #otherwise collude
                 else:
-                    return 'c' #otherwise collude
+                    return 'b'
 
 
 
