2fc08f05b99dcb22553d29154a03f5fad86b733c
TomDerpMcSwaggy <tmtwnsnd@gmail.com>
2014-11-18 11:31:25 -0600

    Aidan Burke-player 12 update

--- prisoners_dilemma.py
+++ prisoners_dilemma.py
@@ -396,18 +396,19 @@ def get_action(player, history, opponent_history, score, opponent_score, getting
     ######
     ######
     #
-    elif player == 12:
+    player == 12:
         if getting_team_name:
-            return 'loyal vengeful'
+            return 'Preists of Nut-Nut'
         else:
             # use history, opponent_history, score, opponent_score
             # to compute your strategy
             if len(opponent_history)==0: #It's the first round: collude
-                return 'c'
+                return 'b'
             elif history[-1]=='c' and opponent_history[-1]=='b':
                 return 'b' # betray is they were severely punished last time
             else:
-                return 'c' #otherwise collude
+                return 'b' #otherwise collude
+    
     
     
 
